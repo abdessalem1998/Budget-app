e0b9f92b65290fa8520dc8de2403fe7284c9269b
Houfaf abdessalem <abdeslam.dib.usa@gmail.com>
2022-05-23 23:28:31 +0100

    model association and validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_many :spendings, foreign_key: 'author_id', dependent: :destroy
+  has_many :types, dependent: :destroy
+  validates :name, presence: true, length: { maximum: 250 }
 end
