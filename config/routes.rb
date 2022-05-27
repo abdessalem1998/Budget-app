ee65127d7e3d36da88076ddd1ba3818b6faf7547
Houfaf abdessalem <abdeslam.dib.usa@gmail.com>
2022-05-27 13:41:03 +0100

    Required Changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :spendings
-  resources :types
-  resources :users
+  resources :spendings, only: %i[index new]
+  resources :types, only: %i[index new]
+  resources :users, only: %i[index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
 
   unauthenticated do
-    root "users#index" 
+    root "users#index"
   end
 
   authenticated :user do
